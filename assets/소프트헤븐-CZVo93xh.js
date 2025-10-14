2bfa0bb6b99366289b8b7db98faf221824b3580e
IKKIson <shout2517@naver.com>
2025-10-14 23:53:26 +0900

    Updates

---
+++ assets/소프트헤븐-CZVo93xh.js
@@ -0,0 +1,16 @@
+const r=`# 소프트헤븐\r
+\r
+**기간:** 2025.09  \r
+**기술 스택:** React, TypeScript, TailwindCSS, Vite\r
+\r
+## 프로젝트 설명\r
+- 개인 Todo 웹 애플리케이션\r
+- Markdown 기반 Todo 관리\r
+- Prev/Next 버튼과 날짜 선택 기능 포함\r
+- 반응형 UI (모바일/데스크톱 지원)\r
+\r
+## 주요 기능\r
+- 매일/매주 Todo 리스트 관리\r
+- 날짜별 Todo 리스트 탐색\r
+- Markdown 파일로 관리하여 콘텐츠 수정 용이\r
+`;export{r as default};
