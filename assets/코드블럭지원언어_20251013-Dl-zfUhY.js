a9e39600e410144414173ec453b8af3633911887
IKKIson <shout2517@naver.com>
2025-10-14 22:50:17 +0900

    Updates

---
+++ assets/코드블럭지원언어_20251013-Dl-zfUhY.js
@@ -0,0 +1,65 @@
+const r=`# 마크다운 코드블럭 지원 언어\r
+\r
+| Language         | Available language mode(s)                                                                 |\r
+|------------------|---------------------------------------------------------------------------------------------|\r
+| ASP.NET          | asp, aspx                                                                                  |\r
+| C                | c                                                                                           |\r
+| C++              | c++, cpp, cplusplus                                                                         |\r
+| C#               | cs, csharp                                                                                  |\r
+| Clojure          | clj, cljc, cljx, clojure                                                                    |\r
+| CSS              | css, less, sass, scss, styl, stylus                                                         |\r
+| cURL             | curl                                                                                        |\r
+| D                | d                                                                                           |\r
+| Dart             | dart                                                                                        |\r
+| Diff             | diff                                                                                        |\r
+| Docker           | dockerfile                                                                                  |\r
+| Erlang           | erl, erlang                                                                                 |\r
+| Go               | go                                                                                          |\r
+| GraphQL          | gql, graphql                                                                                |\r
+| Groovy           | gradle, groovy                                                                              |\r
+| Handlebars       | handlebars, hbs                                                                             |\r
+| HTML/XML         | html, xhtml, xml                                                                            |\r
+| HTTP             | http                                                                                        |\r
+| Java             | java                                                                                        |\r
+| JavaScript       | coffeescript, ecmascript, javascript, js, node                                              |\r
+| JSX              | jsx                                                                                         |\r
+| JSON             | json                                                                                        |\r
+| JSON5            | json5                                                                                       |\r
+| Julia            | jl, julia                                                                                   |\r
+| Kotlin           | kotlin, kt                                                                                  |\r
+| Liquid           | liquid                                                                                      |\r
+| Lua              | lua                                                                                         |\r
+| Markdown         | markdown                                                                                    |\r
+| Mermaid          | mermaid                                                                                     |\r
+| Objective-C      | objc, objectivec                                                                            |\r
+| Objective-C++    | objc++, objcpp, objectivecpp, objectivecplusplus                                            |\r
+| OCaml            | ocaml, ml                                                                                   |\r
+| Perl             | perl, pl                                                                                    |\r
+| PHP              | php                                                                                         |\r
+| PowerShell       | powershell, ps1                                                                             |\r
+| Python           | py, python                                                                                  |\r
+| R                | r                                                                                           |\r
+| React            | jsx                                                                                         |\r
+| Ruby             | jruby, macruby, rake, rb, rbx, ruby                                                         |\r
+| Rust             | rs, rust                                                                                    |\r
+| Scala            | scala                                                                                       |\r
+| Shell            | bash, sh, shell, zsh                                                                        |\r
+| Solidity         | sol, solidity                                                                               |\r
+| SQL              | cql, mssql, mysql, plsql, postgres, postgresql, pgsql, sql, sqlite                          |\r
+| Swift            | swift                                                                                       |\r
+| TypeScript       | ts, typescript                                                                              |\r
+| YAML             | yaml, yml                                                                                   |\r
+| Haskell          | hs, haskell                                                                                 |\r
+| Lisp             | lisp, elisp, emacs, cl                                                                      |\r
+| LaTeX            | tex, latex                                                                                  |\r
+| TOML             | toml                                                                                        |\r
+| INI              | ini, cfg, conf                                                                              |\r
+| MATLAB           | matlab, m                                                                                   |\r
+| Fortran          | f, fortran, f90                                                                             |\r
+| VB.NET           | vb, vbnet                                                                                   |\r
+| Visual Basic     | vbs, visualbasic                                                                            |\r
+| Assembly         | asm, nasm                                                                                   |\r
+| Makefile         | makefile, make                                                                              |\r
+| CSV              | csv                                                                                         |\r
+| Plain Text       | text, plaintext                                                                             |\r
+`;export{r as default};
